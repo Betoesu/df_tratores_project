6e1765011d4e0c893b09918a57a01594e8c3c0b1
Betoesu <pedrosarmento1412@gmail.com>
2026-04-09 16:22:04 -0300

    feat: implementa upload de imagens reais para as peças

--- src/app.py
+++ src/app.py
@@ -1,6 +1,11 @@
+import os
 from flask import Flask, render_template, request, redirect, url_for
+from werkzeug.utils import secure_filename 
 
 app = Flask(__name__)
+
+UPLOAD_FOLDER = 'src/static/img'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 pecas = [
     {   
@@ -34,17 +39,27 @@ def home():
 @app.route('/adicionar', methods=['GET', 'POST'])
 def adicionar_peca():
     if request.method == 'POST':
+
+        arquivo = request.files.get('imagem')
         
+        if arquivo:
+            # Limpa o nome do arquivo e salva na pasta static/img
+            nome_arquivo = secure_filename(arquivo.filename)
+            arquivo.save(os.path.join(app.config['UPLOAD_FOLDER'], nome_arquivo))
+            caminho_imagem = f'img/{nome_arquivo}'
+        else:
+            caminho_imagem = 'img/placeholder.jpg' # Caso não envie foto
+
         nova_peca = {
             'nome': request.form.get('nome'),
             'categoria': request.form.get('categoria'),
             'preco': float(request.form.get('preco')),
-            'imagem': 'img/placeholder.jpg' # Por enquanto deixamos um padrão
+            'imagem': caminho_imagem
         }
 
         pecas.append(nova_peca)
-
         return redirect(url_for('home'))
+    
     return render_template('adicionar.html')
 
 
